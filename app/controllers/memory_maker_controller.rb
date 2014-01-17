cab5bdc6c6a1c57518fe584892bfb10b668882c1
Stephen Kunath <skunath@gmail.com>
2014-01-17 18:32:19 -0500

    fixes

--- app/controllers/memory_maker_controller.rb
+++ app/controllers/memory_maker_controller.rb
@@ -4,6 +4,8 @@ class MemoryMakerController < ApplicationController
     @memory.memory_key = params["memory_key"]
     @memory.memory_val= params["memory_val"]
     @memory.save
+    render_text = "OK"
+    render :text => render_text
   end
 
   def retrieve_memory
